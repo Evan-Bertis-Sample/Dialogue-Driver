62dac17f41ad7890781c5933fb9c49113fdb75d1
Shrimp-Chip <esample21@gmail.com>
2023-06-18 10:12:59 -0400

    Added node_system testing suite, and renamed intenral testing suite to fact_system

---
+++ tests/node_system/node_tests.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+#include "gtest/gtest.h"
+#include "dialogue_driver/fact.h"
+#include "dialogue_driver/fact_collection.h"
+#include "dialogue_driver/icriteria.h"
+#include "dialogue_driver/operator.h"
+#include "dialogue_driver/expression.h"
+#include "dialogue_driver/combined_criteria.h"
+
+TEST(Nodes, Test)
+{
+    assert(true);
+}
